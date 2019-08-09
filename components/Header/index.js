2bfa47d6a0679433640aef691b5bee0aafc41c01
Jonathan Ho <johnnyho82@gmail.com>
2019-08-09 11:22:57 -0400

    Define new header elements

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,16 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+  // Define new elements
+  const header = document.createElement('div');
+  const date = document.createElement('span');
+  const lambdaTimes = document.createElement('h1');
+  const temp = document.createElement('span');
+
+  // Set up structure of elements
+
+  // Set class names
+
+  // Set content
+}
