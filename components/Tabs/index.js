58799410e911adda552b1a88e1342d7b9e277976
Jonathan Ho <johnnyho82@gmail.com>
2019-08-09 11:41:22 -0400

    Create individual Tabs components

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,7 +8,15 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-axios.get('https://lambda-times-backend.herokuapp.com/topics');
+axios
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response =>
+    response.data.topics.forEach(element => {
+      const topics = document.querySelector('.topics');
+      topics.append(createTab(element));
+    }),
+  )
+  .catch(err => console.log(err));
 
 function createTab(topic) {
   const tab = document.createElement('div');
